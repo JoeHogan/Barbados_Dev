a0ea5e6d79673a3ef7174aa40dbea1954c6b1527
Joe Hogan <joseph.t.hogan@gmail.com>
2015-12-18 13:06:37 -0500

    updated form data. added support for typeahead dropdowns

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 ﻿(function(){
 
-	var app = angular.module('app', ['ui.bootstrap','dataService'])
+	var app = angular.module('app', ['ui.bootstrap','dataService','ui.select'])
 	
 	.config(['$httpProvider', function ($httpProvider) {
 		$httpProvider.defaults.headers.post['Content-Type'] = 'application/json;odata=verbose';
@@ -118,6 +118,37 @@
 				});
 			}
 		}
+	}])
+
+	.filter('propsFilter', [function() {
+  		return function(items, props) {
+    		var out = [];
+
+		    if (angular.isArray(items)) {
+		      items.forEach(function(item) {
+		        var itemMatches = false;
+
+		        var keys = Object.keys(props);
+		        for (var i = 0; i < keys.length; i++) {
+		          var prop = keys[i];
+		          var text = props[prop].toLowerCase();
+		          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+		            itemMatches = true;
+		            break;
+		          }
+		        }
+
+		        if (itemMatches) {
+		          out.push(item);
+		        }
+		      });
+		    } else {
+		      // Let the output be the input untouched
+		      out = items;
+		    }
+
+    		return out;
+  		}
 	}])
 
 	.directive('deleteDoc', [function(){
